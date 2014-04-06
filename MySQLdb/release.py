6c22fa0de38b434b2981bb25df3163d7cf4a5ffc
Buck Golemon <workitharder@gmail.com>
2014-04-05 18:18:02 -0700

    make the version double-check happy

--- MySQLdb/release.py
+++ MySQLdb/release.py
@@ -2,5 +2,5 @@ from __future__ import unicode_literals
 from __future__ import explicit_encoding
 
 __author__ = "Andy Dustman <farcepest@gmail.com>"
-version_info = (1, 2, 4, 'final', 1)
+version_info = (1, 2, 4, u'final', 1)
 __version__ = "1.2.4"
